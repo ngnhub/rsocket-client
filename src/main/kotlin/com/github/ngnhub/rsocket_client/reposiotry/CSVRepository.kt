21efeac88f4d05b987cb415d59d98cd7f42b7e88
ngnhub <nagovicynalexandr@gmail.com>
2024-02-11 23:46:20 +0100

    refactor repository

--- src/main/kotlin/com/github/ngnhub/rsocket_client/reposiotry/CSVRepository.kt
+++ src/main/kotlin/com/github/ngnhub/rsocket_client/reposiotry/CSVRepository.kt
@@ -2,70 +2,39 @@ package com.github.ngnhub.rsocket_client.reposiotry
 
 import com.github.ngnhub.rsocket_client.model.RSocketInitRequest
 import com.github.ngnhub.rsocket_client.model.SavedRSocketRequest
-import jakarta.annotation.PostConstruct
+import com.github.ngnhub.rsocket_client.utils.CsvBridge
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.sync.Mutex
-import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVParser
 import org.apache.commons.csv.CSVRecord
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.io.File
-import java.io.FileInputStream
-import java.nio.charset.StandardCharsets
 import java.time.LocalDateTime
-import kotlin.reflect.full.declaredMemberProperties
 
+// TODO: cache
 @Service
-class CSVRepository(@Value("\${history.csv.path}") private var path: String) {
+class CSVRepository(val bridge: CsvBridge) {
 
     private val mutex = Mutex()
 
-    companion object {
-        private val headers = SavedRSocketRequest::class.declaredMemberProperties
-            .map {
-                it.name
-            }.toTypedArray()
-    }
-
-    @PostConstruct
-    fun initTable() {
-        val file = File(path)
-        if (!file.exists()) {
-            throw IllegalArgumentException("CSV file not found")
-        }
-        CSVFormat.DEFAULT. print(file, StandardCharsets.UTF_8).use { // FIXME: it rewrites lines
-            it.printRecord(
-                "host",
-                "port",
-                "route",
-                "savedAt"
-            )
-        }
-    }
-
     suspend fun save(request: SavedRSocketRequest): RSocketInitRequest {
         mutex.lock()
-        val file = File(path)
-        CSVFormat.DEFAULT.print(file, StandardCharsets.UTF_8).use {
+        bridge.getCsvPrinter().use {
             with(request) {
-                it .printRecord(
+                it.printRecord(
                     rSocketInitRequest.host,
                     rSocketInitRequest.port,
                     rSocketInitRequest.route,
                     savedAt.toString()
                 )
-                return rSocketInitRequest.also { mutex.unlock() }
+                return rSocketInitRequest
+                    .also { mutex.unlock() }
             }
         }
     }
 
     suspend fun getHistory(): Flow<SavedRSocketRequest> {
         mutex.lock()
-        val file = File(path)
-        val inputStream = FileInputStream(file)
-        return getCsvParser(inputStream).use { parser ->
+        return bridge.getCsvParser().use { parser ->
             parser.drop(1)
                 .map {
                     it.mapToSavedRSocketRequest()
@@ -75,13 +44,6 @@ class CSVRepository(@Value("\${history.csv.path}") private var path: String) {
                 .asFlow()
         }
     }
-
-    private fun getCsvParser(inputStream: FileInputStream): CSVParser =
-        CSVFormat.Builder.create(CSVFormat.DEFAULT)
-            .apply {
-                setIgnoreSurroundingSpaces(true)
-            }
-            .build().parse(inputStream.reader())
 
     private fun CSVRecord.mapToSavedRSocketRequest(): SavedRSocketRequest {
         val rSocketInitRequest = RSocketInitRequest(
---
+++ src/main/kotlin/com/github/ngnhub/rsocket_client/utils/CsvBridge.kt
@@ -0,0 +1,58 @@
+package com.github.ngnhub.rsocket_client.utils
+
+import jakarta.annotation.PostConstruct
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import org.apache.commons.csv.CSVPrinter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+
+@Component
+class CsvBridge(@Value("\${history.csv.path}") private var path: String) {
+
+
+    @PostConstruct
+    fun initFile() {
+        val file = File(path)
+        if (!file.exists()) {
+            throw IllegalArgumentException("CSV file not found")
+        }
+        var size: Int
+        getCsvParser().use {
+            size = it.count()
+        }
+        if (size == 0) {
+            val csvPrinter = getCsvPrinter()
+            csvPrinter.use {
+                it.printRecord("host", "port", "route", "savedAt")
+            }
+        }
+    }
+
+    fun getCsvPrinter(): CSVPrinter =
+        CSVFormat.Builder.create(CSVFormat.DEFAULT)
+            .build()
+            .print(getWriter())
+
+    fun getCsvParser(): CSVParser =
+        CSVFormat.Builder.create(CSVFormat.DEFAULT)
+            .apply {
+                setSkipHeaderRecord(true)
+                setIgnoreSurroundingSpaces(true)
+            }
+            .build().parse(FileInputStream(File(path)).reader())
+
+    private fun getWriter(): BufferedWriter {
+        return Files.newBufferedWriter(
+            Paths.get(path),
+            StandardOpenOption.APPEND,
+            StandardOpenOption.CREATE
+        )
+    }
+}
