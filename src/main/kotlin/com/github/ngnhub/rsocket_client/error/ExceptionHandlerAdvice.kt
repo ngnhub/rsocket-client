6dec3d9df00e7e941a991be5f7c217861bd0aeef
ngnhub <nagovicynalexandr@gmail.com>
2024-02-14 18:33:16 +0100

    input exception handler

--- src/main/kotlin/com/github/ngnhub/rsocket_client/error/ExceptionHandlerAdvice.kt
+++ src/main/kotlin/com/github/ngnhub/rsocket_client/error/ExceptionHandlerAdvice.kt
@@ -1,10 +1,15 @@
 package com.github.ngnhub.rsocket_client.error
 
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.ServerWebInputException
+import reactor.core.publisher.Mono
 
 
 @ControllerAdvice
@@ -27,5 +32,17 @@ class ExceptionHandlerAdvice : ResponseEntityExceptionHandler() {
         log.error(LOG_TAG, exc)
         return ResponseEntity.internalServerError()
             .body(exc.message)
+    }
+
+    override fun handleServerWebInputException(
+        ex: ServerWebInputException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        exchange: ServerWebExchange
+    ): Mono<ResponseEntity<Any>> {
+        log.error(LOG_TAG, ex.cause)
+        val body: ResponseEntity<Any> = ResponseEntity.internalServerError()
+            .body(ex.cause?.message)
+        return Mono.just(body)
     }
 }
