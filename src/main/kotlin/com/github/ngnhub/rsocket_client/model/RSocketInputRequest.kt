a3cdfa5e23232ce8f75e9881495005ffe0c1f978
ngnhub <nagovicynalexandr@gmail.com>
2024-02-22 16:23:04 +0100

    Revert "allow empty route"
    This reverts commit 578a1129694014c683c52f9810016819fbd6bd3e.

--- src/main/kotlin/com/github/ngnhub/rsocket_client/model/RSocketInputRequest.kt
+++ src/main/kotlin/com/github/ngnhub/rsocket_client/model/RSocketInputRequest.kt
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.NotNull
 data class RSocketInputRequest(
     @field:NotNull val host: String?,
     @field:NotNull val port: Int?,
-    val route: String?
+    @field:NotNull val route: String?
 )
