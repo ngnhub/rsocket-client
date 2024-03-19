f18dfd88d26add8d3c2db7853a4723ec83f61aff
ngnhub <nagovicynalexandr@gmail.com>
2024-03-19 15:36:30 +0100

    fix controller bean cration

--- web/src/main/kotlin/com/github/ngnhub/rsocket_client/web/RsocketClientApplication.kt
+++ web/src/main/kotlin/com/github/ngnhub/rsocket_client/web/RsocketClientApplication.kt
@@ -1,10 +1,11 @@
 package com.github.ngnhub.rsocket_client.web
 
 import com.github.ngnhub.rsocket_client.domain.config.DomainAutoConfiguration
+import com.github.ngnhub.rsocket_client.web.config.WebAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackageClasses = [DomainAutoConfiguration::class])
+@SpringBootApplication(scanBasePackageClasses = [DomainAutoConfiguration::class, WebAutoConfiguration::class])
 class RsocketClientApplication
 
 fun main(args: Array<String>) {
---
+++ web/src/main/kotlin/com/github/ngnhub/rsocket_client/web/config/WebAutoConfiguration.kt
@@ -0,0 +1,9 @@
+package com.github.ngnhub.rsocket_client.web.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("com.github.ngnhub.rsocket_client.web")
+class WebAutoConfiguration {
+}
